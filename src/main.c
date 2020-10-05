b010298bc04daf0ab2c5ee1dffe3a78f2c934615
emiliollbb <emiliollbb@gmail.com>
2020-10-05 18:50:10 +0200

    Initial

--- src/main.c
+++ src/main.c
@@ -1,15 +1,27 @@
 #include <genesis.h>
+#include "version.h"
+
+void run_intro(void);
+
+void run_intro() {
+	u16 i;
+	
+	VDP_drawText("WITCH", 10, 13);
+	VDP_drawText(VERSION, 1, 27);
+	
+	for(i=0; i<150; i++) {
+		VDP_waitVSync();
+	}
+}
 
 int main(void)
 {
-    VDP_drawText("Hello Genny World!", 10, 13);
+    run_intro();
+    VDP_resetScreen();
 
     while(TRUE)
     {
-        // read input
-        // move sprite
-        // update score
-        // draw current screen (logo, start screen, settings, game, gameover, credits...)
+        VDP_waitVSync();
     }
 
     return 0;
