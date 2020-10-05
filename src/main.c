65b19180add5619f548154e34a5e9a26153227ee
emiliollbb <emiliollbb@gmail.com>
2020-10-05 18:33:33 +0200

    Initial

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <genesis.h>
+
+int main(void)
+{
+    VDP_drawText("Hello Genny World!", 10, 13);
+
+    while(TRUE)
+    {
+        // read input
+        // move sprite
+        // update score
+        // draw current screen (logo, start screen, settings, game, gameover, credits...)
+    }
+
+    return 0;
+}
+
